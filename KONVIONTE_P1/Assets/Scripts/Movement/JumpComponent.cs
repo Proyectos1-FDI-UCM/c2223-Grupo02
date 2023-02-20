405a67b6823d5eaad55f8211fb40739c2558da0c
Jimbo <javicentear@gmail.com>
2023-02-20 21:02:37 +0100

    Prototipo de salto terminado
    Piola

--- KONVIONTE_P1/Assets/Scripts/Movement/JumpComponent.cs
+++ KONVIONTE_P1/Assets/Scripts/Movement/JumpComponent.cs
@@ -22,18 +22,11 @@ public class JumpComponent : MonoBehaviour
     #endregion
     #region Prarameters
     [SerializeField]
-    [Tooltip("Velocidad horizontal del salto")]
-    private float _footSpeed;
-    [SerializeField]
     [Tooltip("Altura del Salto")]
     private float _heightToPeak;
     [SerializeField]
-    [Tooltip("Distancia a la que está la abside de la parábola")]
-    private float _distanceToPeak;
-
-    [Tooltip("Impulso de salto inicial")]
-    [SerializeField]
-    private float _jumpForceVal;
+    [Range(0f, 1f)]
+    private float _speedDivider;
 
     [Header("Tiempos de subida y bajada")]
     [SerializeField]
@@ -43,18 +36,11 @@ public class JumpComponent : MonoBehaviour
 
     #endregion
     #region Properties
-    [Space(15)]
-    [SerializeField]
+    private bool _canceled;
     private float _velocity;
-    [SerializeField]
     private float _position;
-    [SerializeField]
     private float _gravity;
-
-
     private float _fallSpeed;
-
-    [SerializeField]
     private float _upIniSpeed;
     #endregion
 
@@ -64,41 +50,56 @@ public class JumpComponent : MonoBehaviour
         _myTransform = transform;
         _actions = new PlayerInputActions();
         _actions.Player.Enable();
-        _gravity = - (2 * _heightToPeak * Mathf.Pow(_footSpeed,2)) / Mathf.Pow(_distanceToPeak, 2);
+        _gravity = - (2 * _heightToPeak) / Mathf.Pow(_ascensionTime, 2);
 
-        //la velocidad de bajada depende del tiempo y la altura a la que queramos llegar
+        // La velocidad de bajada depende del tiempo y la altura a la que queramos llegar
         _fallSpeed = _heightToPeak / _descensionTime;
+        _upIniSpeed = (2 * _heightToPeak) / _ascensionTime;
     }
+
     //fixed update para regular la gravedad
     private void FixedUpdate()
     {
-        //gravedad Magia de la física de la ESO
-        
-        _velocity += _gravity * Time.fixedDeltaTime;
-        _position = _velocity * Time.fixedDeltaTime + 0.5f * _gravity * Mathf.Pow(Time.fixedDeltaTime, 2);
-        _myTransform.position += _position * (Vector3)Vector2.up;
-        
-        if (_velocity< 0)
+        // IMPORTANTE Para testear en ejecucion
+        // _upIniSpeed = (2 * _heightToPeak) / _ascensionTime;
+        // Gravedad Magia de la física de la ESO
+        if (_velocity < 0 ||
+            (_canceled && _velocity < (_upIniSpeed * _speedDivider)))
         {
             Gravity();
         }
+        else
+        {          
+            _velocity -= Time.fixedDeltaTime * _upIniSpeed / _ascensionTime;
+            _position = _velocity * Time.fixedDeltaTime + 0.5f * _gravity * Mathf.Pow(Time.fixedDeltaTime, 2);
+            _myTransform.position += _position * (Vector3)Vector2.up;
+        }
     }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            _gravity = - (2 * _heightToPeak * Mathf.Pow(_footSpeed, 2)) / Mathf.Pow(_distanceToPeak, 2);
-            _velocity = _jumpForceVal + _gravity * Time.deltaTime;
+            _gravity = - (2 * _heightToPeak) / Mathf.Pow(_ascensionTime, 2);
+            _velocity = _upIniSpeed;
+            _canceled = false;
+        }
+        if (context.canceled)
+        {
+            _canceled = true;
         }
     }
 
     public void Gravity()
     {
-        _myTransform.position -= Vector3.up * _fallSpeed*Time.fixedDeltaTime;
+        Debug.Log("tuviejaGravity");
+        _myTransform.position -= Vector3.up * _fallSpeed * Time.fixedDeltaTime;
     }
+
     private void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("Tuvieja");
+
+        Debug.Log("TuviejaTrigger");
         _velocity = 0;
         _gravity = 0;
         _position = 0;
