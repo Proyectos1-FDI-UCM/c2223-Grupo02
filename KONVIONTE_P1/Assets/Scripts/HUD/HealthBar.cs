838c9e263416c003a648671555f79d9804a51a13
M00n-Hun7r3ss <denisaju@ucm.es>
2023-02-23 20:09:01 +0100

    HealthBar attached to LifeComponent
    Solo funciona para el Player, de momento.
    Falta attacharlo también al AttackComponent

--- KONVIONTE_P1/Assets/Scripts/HUD/HealthBar.cs
+++ KONVIONTE_P1/Assets/Scripts/HUD/HealthBar.cs
@@ -11,7 +11,7 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Slider _slider;
 
     [Tooltip("Vida máxima")]
-    [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private int _maxHealth;
 
     #endregion
 
@@ -22,17 +22,17 @@ public class HealthBar : MonoBehaviour
 
     #endregion
 
-    #region Methods for HealthBar
+    #region Methods
 
     //Este método ajusta el valor de la vida 
-    private void SetHealth()
+    public void SetHealth()
     { 
         //Asignamos el valor del slider al de la vida del personaje
         _slider.value = _currentHealth; 
     }
 
     //Este método setea una vida máxima
-    private void SetMaxHealth()
+    public void SetMaxHealth()
     {
         //Asignamos el valor máximo del slider
         _slider.maxValue = _maxHealth;
@@ -43,56 +43,7 @@ public class HealthBar : MonoBehaviour
 
     #endregion
 
-    #region Methods for LifeComponent
-
-    //Este método se encarga de bajar la vida cuando recibe daño el jugador
-    private void TakeDamage(int _damage)
-    {
-        //Bajamos la vida del jugador
-        _currentHealth -= _damage;
-
-        //En LifeComponent, llamar a la barra de vida, y setearla al current
-        //HealthBar.SetHealth();
-
-    }
-
-    //Este método se encarga de devolver vida cuando el jugador coge una cura
-    private void TakeCure(int _cure)
-    {
-        //Si la vida del jugador no es máxima
-        if (_currentHealth < _maxHealth )
-        { 
-            //Sumará el valor de la cura
-            _currentHealth += _cure; 
-        }
-
-        //Recordar que la cura tiene su propio script que 
-        //se encarga de su funcionamiento.
-        //Aquí solo lo movemos en la barrita.
-    }   
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Al empezar, la vida es máxima
-        _currentHealth = _maxHealth;
-
-        //En LifeComponent, llamar a la barra de vida, y setearla al máximo
-        //HealthBar.SetMaxHealth();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Depende del LifeComponent de Manolo;
-
-        //Si le atacan, llamar a TakeDamage(valor del daño)
-
-        //Si coje cura, llamar a TakeCure(valor de la cura)
-
-    }
-
-    #endregion
+    
 
 
 
