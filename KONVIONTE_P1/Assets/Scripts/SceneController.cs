c22640759a4cbbc7343bd85c67528571ab1b97fc
M00n-Hun7r3ss <denisaju@ucm.es>
2023-03-06 20:39:51 +0100

    Scene Controller
    Ya cambia de escenas al pulsar el botón.
    
    Falta poner el flujo de juego

---
+++ KONVIONTE_P1/Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    #region Methods
+    //Se llama al número de escena asignado en Build Settings
+    public void ChangeScene(string sceneName)
+    {
+        Debug.Log("TuVieja");
+        SceneManager.LoadScene(sceneName);
+
+    }
+    #endregion
+}
