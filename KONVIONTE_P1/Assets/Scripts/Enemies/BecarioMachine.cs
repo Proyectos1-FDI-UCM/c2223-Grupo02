b983d8b1bdaab51d76798ee0ea9b081c9e8f33c8
samuel <samuga05@ucm.es>
2023-03-17 12:42:53 +0100

    Cositas de estados prueba

--- KONVIONTE_P1/Assets/Scripts/Enemies/BecarioMachine.cs
+++ KONVIONTE_P1/Assets/Scripts/Enemies/BecarioMachine.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BecarioMachine : StateMachine
 {
+    Transform _myTransform;
+    MovementComponent _myMovement;
+    Transform _playerTransform;
+
+
+
+
+
+
+    PatrollState patrollState;
+
+
+    Transition detectaEnemigo;
+    Func<bool> _detectaEnemigo;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _detectaEnemigo = () => DetectaEnemigo(_myTransform);
+
+        patrollState = new PatrollState(_myTransform, _myMovement, _playerTransform);
+
+
+
+        //InicializaTransicion(patrollState,,);
     }
 
     // Update is called once per frame
     void Update()
     {
         Tick();
+    }
+
+
+    public bool DetectaEnemigo(Transform a)
+    {
+        return true;
     }
 }
